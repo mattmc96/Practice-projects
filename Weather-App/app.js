a475620bb4502ce05f9eed9ff395f8fbf98c8cac
Matthew Mccarthy <68124857+mattmc96@users.noreply.github.com>
2020-07-25 12:05:56 -0500

    Add files via upload

---
+++ Weather-App/app.js
@@ -0,0 +1,16 @@
+
+function calculateTaxes(price, quantity) {
+    var salesTax = .10;
+    var totalPrice;
+    // Only change code below this line
+    totalPrice = salesTax * (price * quantity);
+    
+
+    // Only change code above this line
+    return totalPrice;
+}
+
+// Change the inputs below to test your code
+calculateTaxes(1, 10);
+
+
